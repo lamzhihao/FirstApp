3684352cc6b9bbd46d539cdcef14e869a62740f4
doubleristretto <doubleristretto@noreply.github.com>
2021-01-18 23:04:19 +0800

    commit lab 1 skeleton code

---
+++ README.md
@@ -0,0 +1,2 @@
+# ict2105-lab01-2021
+A simple app project
---
+++ app/src/main/java/edu/singaporetech/firstapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package edu.singaporetech.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Lab 01: My First App
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //TODO: add your logging message here.
+
+    }
+}
